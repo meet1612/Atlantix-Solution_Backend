5f59a894ecd8ad7cb207f5a7014ba4088d2d718b
meet1612 <meetjshah82@gmail.com>
2020-08-06 14:39:06 +0530

    change in employee, images stored

--- model/employee_model.js
+++ model/employee_model.js
@@ -2,16 +2,16 @@ var db=require('../dbconnection');
 var employee={
     
     getAllEmployee:function(callback){
-        return db.query("select e.e_mobile,e.aadharcard_no,e.e_name,e.e_address,e.e_pincode,e.e_workingstatus,s.s_name,e.e_addedAt,e.e_updatedAt,e.e_status from employee_tbl e,service_tbl s WHERE e.s_id=s.s_id",callback);
+        return db.query("select e.e_mobile,e.aadharcard_no,e.e_name,e.e_image,e.e_address,e.e_pincode,e.e_workingstatus,s.s_name,e.e_addedAt,e.e_updatedAt,e.e_status from employee_tbl e,service_tbl s WHERE e.s_id=s.s_id",callback);
     },
 
     getEmployeeById:function(mobile_no,callback){
         return db.query("select * from employee_tbl where e_mobile=?",[mobile_no],callback);
     },
 
-    addEmployee:function(item,callback){
+    addEmployee:function(item,filename,callback){
         var d=new Date();
-        return db.query("insert into employee_tbl values(?,?,?,?,?,?,?,?,?,?)",[item.e_mobile,item.aadharcard_no,item.e_name,item.e_address,item.e_pincode,item.e_workingstatus,item.s_id,d,d,item.e_status],callback);
+        return db.query("insert into employee_tbl values(?,?,?,?,?,?,?,?,?,?,?)",[item.e_mobile,item.aadharcard_no,item.e_name,filename,item.e_address,item.e_pincode,item.e_workingstatus,item.s_id,d,d,item.e_status],callback);
     },
 
     updateEmployee:function(mobile_no,item,callback){
