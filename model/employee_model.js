9f6fe65bde25b549465a8a8b0389a2fd095f2a7c
sheilja <62593034+sheilja@users.noreply.github.com>
2020-08-08 13:13:58 +0530

    employeeService_tbl add delete query

--- model/employee_model.js
+++ model/employee_model.js
@@ -17,6 +17,13 @@ var employee={
         var d=new Date();
         return db.query("INSERT INTO employee_tbl(e_mobile, aadharcard_no, e_name, e_image, e_address, e_pincode, e_workingstatus, e_addedAt, e_updatedAt, e_status) VALUES (?,?,?,?,?,?,?,?,?,?)",[item.e_mobile, item.aadharcard_no, item.e_name, filename, item.e_address, item.e_pincode, item.e_workingstatus, d, d, item.e_status],callback);
     },
+    addEmployeeService:function(item,filename,callback){
+        var d=new Date();
+        return db.query("INSERT INTO employeeservice_tbl(es_id, e_mobile, s_id) VALUES (?,?,?)",[item.es_id, item.e_mobile, item.s_id],callback);
+    },
+    deleteEmployeeService:function(es_id,callback){
+        return db.query("delete from employeeservice_tbl where es_id=?",[es_id],callback);
+    },
 
     updateEmployee:function(mobile_no,item,callback){
         var d=new Date();
@@ -27,6 +34,7 @@ var employee={
         var d=new Date();
         return db.query("update employee_tbl set  e_updatedAt=? ,e_status=1 where e_mobile=?",[d,mobile_no],callback);
     }
+
 };
 
 module.exports=employee;
