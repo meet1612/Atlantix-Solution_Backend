45a11825a5013e3666e0f64db96f680e5396639d
Shrey Patel <35284858+Shrey11599@users.noreply.github.com>
2020-08-10 22:45:32 +0530

    Update packagepurchase_model.js

--- model/packagepurchase_model.js
+++ model/packagepurchase_model.js
@@ -1,14 +1,20 @@
 var db=require('../dbconnection');
 var packagePurchase={
     
-    getAllPackagePurchase:function(callback){
-        return db.query("select pp.pp_id,pp.p_mobile,pp.pp_date,pp.pp_amount,p.pk_name,pp.pp_endDate,pp.pp_status from packagepurchase_tbl pp,package_tbl p WHERE pp.pk_id=p.pk_id",callback);
-    },
+    getAllOrdermaintain: function (callback) {
+    return db.query(
+      "SELECT om.om_id, p.pk_name, pd.p_name,pd.p_mobile, s.s_name ,om.om_status,om.e_mobile,e.e_name FROM ordermaintain_tbl om, packagepurchase_tbl pp, service_tbl s,package_tbl p, persondetail_tbl pd,employee_tbl e WHERE om.pp_id = pp.pp_id AND p.pk_id = pp.pk_id AND pp.p_mobile = pd.p_mobile AND om.e_mobile=e.e_mobile AND om.s_id = s.s_id AND om.om_status = 1 ",
+      callback
+    );
+  },
 
-    getPackagePurchaseById:function(id,callback){
-        return db.query("select * from packagepurchase_tbl where pp_status=0 and pp_id=?",[id],callback);
-    },
-
+  getAllOrdermaintainById: function (om_id, callback) {
+    return db.query(
+      "SELECT om.om_id, p.pk_name, pd.p_name,pd.p_mobile, s.s_name ,om.om_status,om.e_mobile,e.e_name FROM ordermaintain_tbl om, packagepurchase_tbl pp, service_tbl s,package_tbl p, persondetail_tbl pd,employee_tbl e WHERE om.pp_id = pp.pp_id AND p.pk_id = pp.pk_id AND pp.p_mobile = pd.p_mobile AND om.e_mobile=e.e_mobile AND om.s_id = s.s_id AND om_id=?",
+      [om_id],
+      callback
+    );
+  },
     addPackagePurchase:function(item,callback){
         var d=new Date();
         return db.query("insert into packagepurchase_tbl values(?,?,?,?,?,?,?)",[item.pp_id,item.p_mobile,d,item.pp_amount,item.pk_id,item.pp_endDate,item.pp_status],callback);
