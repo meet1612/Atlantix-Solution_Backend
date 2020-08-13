804625841d57932131c9c58d2445df274159a7f8
prachi <prachisoni24599@gmail.com>
2020-08-13 19:36:56 +0530

    msg

--- routes/getServiceStatus_route.js
+++ routes/getServiceStatus_route.js
@@ -2,8 +2,8 @@ var servicestatus = require("../model/ordermaintain_model");
 var express = require("express");
 var router = express.Router();
 
-router.get('/:pk_id/:pp_endDate/:p_mobile',function(req,res,next){
-    servicestatus.getServiceStatus(req.params.pk_id,req.params.pp_endDate,req.params.p_mobile,function(err,rows){
+router.get('/:pk_id/:p_mobile',function(req,res,next){
+    servicestatus.getServiceStatus(req.params.pk_id,req.params.p_mobile,function(err,rows){
           if(err){
               res.json(err);
           }
