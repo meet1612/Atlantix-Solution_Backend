86d783e9e2554531a75563a9064881a26e1cd0e8
Shrey Patel <35284858+Shrey11599@users.noreply.github.com>
2020-08-15 11:55:59 +0530

    Update employee_model.js

--- model/employee_model.js
+++ model/employee_model.js
@@ -39,7 +39,7 @@ var employee={
 
     getEmployeeRemainingServiceById:function(mobile_no,callback){
         return db.query("select s_name from service_tbl where s_status=0 and s_name NOT IN (select s_name from service_tbl s, employee_tbl e, employeeservice_tbl es where s.s_id=es.s_id and e.e_mobile=es.e_mobile and e.e_mobile=? and s_status=0 and es_status=0)",[mobile_no],callback);
-    }
+    },
     
      // this function also checks the current working status of the employee
   getEmployeeByServiceID: function (ID, callback) {
