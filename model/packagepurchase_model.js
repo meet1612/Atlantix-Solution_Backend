a62ddbc0c734443ad9f11768aeb7b1760589093d
Shrey Patel <35284858+Shrey11599@users.noreply.github.com>
2020-08-15 12:18:40 +0530

    Update packagepurchase_model.js

--- model/packagepurchase_model.js
+++ model/packagepurchase_model.js
@@ -2,7 +2,7 @@ var db=require('../dbconnection');
 var packagePurchase={
     
     getAllPackagePurchase:function(callback){
-        return db.query("select pp.pp_id,pp.p_mobile,pp.pp_date,pp.pp_amount,p.pk_name,pp.pp_endDate,pp.pp_status from packagepurchase_tbl pp,package_tbl p WHERE pp.pk_id=p.pk_id",callback);
+        return db.query("select pp.pp_id,pp.p_mobile,pp.pp_date,pp.pp_amount,p.pk_name,pp.pp_endDate from packagepurchase_tbl pp,package_tbl p WHERE pp.pk_id=p.pk_id",callback);
        
     },
 
